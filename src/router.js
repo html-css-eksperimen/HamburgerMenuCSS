fba91ef8be85caa706fad47a4b1a9614fe612ed0
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-04-24 09:52:46 +0700

    Pembuatan awal halaman utam

--- src/router.js
+++ src/router.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-// TODO: Membuat halaman baru
+const HomeHamburger = () => import(/* webpackChunkName: "hamburger-home" */'./views/mainmenu/MainMenu.vue');
+
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: HomeHamburger,
     },
     {
       path: '/about',
---
+++ src/views/mainmenu/MainMenu.vue
@@ -0,0 +1,3 @@
+<template src="./main-menu.html"></template>
+<script src="./main-menu.js"></script>
+<style src="./main-menu.css"></style>
